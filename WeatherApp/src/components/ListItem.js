46f7ac8f2ad78f2952a2463e6b41d442b9ead19a
DamiánQuezada <damianquezada.s@gmail.com>
2023-05-15 15:02:01 -0400

    Minor Fix to UpcomingWeather Screen

--- WeatherApp/src/components/ListItem.js
+++ WeatherApp/src/components/ListItem.js
@@ -5,14 +5,14 @@ import { weatherType } from "../utilities/weatherType";
 import moment from "moment";
 
 const ListItem = (props) => {
-  const { dt_text, min, max, condition } = props;
+  const { dt_txt, min, max, condition } = props;
   const { item, temp, date, dateTextWrapper } = styles;
   return (
     <View style={item}>
       <Feather name={weatherType[condition]?.icon} size={50} color={"white"} />
       <View style={dateTextWrapper}>
-        <Text style={date}>{moment(dt_text).format('dddd')}</Text>
-        <Text style={date}>{moment(dt_text).format('h:mm:ss a')}</Text>
+        <Text style={date}>{moment(dt_txt).format('dddd')}</Text>
+        <Text style={date}>{moment(dt_txt).format('h:mm:ss a')}</Text>
       </View>
       <Text style={temp}>{`${Math.round(min)}° / ${Math.round(max)}°`}</Text>
     </View>
